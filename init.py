36495cdff493008f158c7144bfb3e56863c1784a
faizan-tariq-confiz <faizan.tariq@confiz.com>
2024-04-21 01:42:50 -0500

    Adding new feature

---
+++ init.py
@@ -0,0 +1,7 @@
+Certainly! Here is the Python code to print 'Hello Jarvis':
+
+```python
+print('Hello Jarvis')
+``` 
+
+You can run this code in a Python environment to see the output. Let me know if you need further assistance.
