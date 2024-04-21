64f9389cbd5485734e97d4359e42b5342792e6ff
faizan-tariq-confiz <faizan.tariq@confiz.com>
2024-04-21 01:41:04 -0500

    Adding new feature

---
+++ init.py
@@ -0,0 +1,7 @@
+Certainly! Here is a simple Python code to print "Hello, World!":
+
+```python
+print("Hello, World!")
+``` 
+
+You can copy and paste this code into a Python environment to see the output.
