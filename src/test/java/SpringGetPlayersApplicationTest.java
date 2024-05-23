83a43e98fcb9ad3281ebb91140756e38bf2e66c2
Usman <usman.rasheed@confiz.com>
2024-05-23 12:11:49 +0500

    auto generated unit test

---
+++ src/test/java/SpringGetPlayersApplicationTest.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class SpringGetPlayersApplicationTest {
+
+    @Test
+    public void testMain() {
+        // Test the main method of SpringGetPlayersApplication
+        String[] args = {};
+        SpringGetPlayersApplication.main(args);
+        // No assertion as it's a void method
+    }
+}
